9206308e75bd78bdbb40ead5d95e4dce59a8aeb1
valodax <snitzel098@gmail.com>
2023-06-14 22:06:52 -0500

    :white_check_mark: `quicksave`

---
+++ foundry-defi-stablecoin/README.md
@@ -0,0 +1,8 @@
+1. (Relative Stability) Anchored or Pegged -> $1.00
+   1. Chainlink Price Feed
+   2. Set a function to exchange ETH & BTC.
+2. Stability Mechanism (minting): Algorithmic (decentralized).
+   1. People can only mint the stablecoin with enough collateral.
+3. Collateral: Exogenous (Crypto collateral)
+   1. wETH
+   2. wBTC
---
+++ foundry-defi-stablecoin/src/DSCEngine.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/*
+ * @title DSCEngine
+ * @author Mitchell Spencer
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg.
+ */
+
+contract DSCEngine {
+
+}
